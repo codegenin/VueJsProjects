d65e89a87dcbb2eae4b6353c30f1ac2711f371e4
James Puro <james.louie.puro@gmail.com>
2018-08-16 22:56:14 +0800

    Add layout and firebase fore geolocation

---
+++ geolocation/src/firebase/init.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCcaMrGW7kcufUxH0x6Vw4c5Qcukgig874",
+  authDomain: "geolocation-b33ea.firebaseapp.com",
+  databaseURL: "https://geolocation-b33ea.firebaseio.com",
+  projectId: "geolocation-b33ea",
+  storageBucket: "geolocation-b33ea.appspot.com",
+  messagingSenderId: "388311639119"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
+export default firebaseApp.firestore();
